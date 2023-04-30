0847b472a517b457df8233cf6da1f7245e0f0c35
Craig Wilson <cw66786@gmail.com>
2023-04-30 07:55:15 -0500

    done and working but want to make it better

--- src/snakeGame.java
+++ src/snakeGame.java
@@ -1,5 +1,6 @@
 public class SnakeGame {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        new GameFrame();
     }
 }
