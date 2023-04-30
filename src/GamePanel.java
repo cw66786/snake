2965ccb1be83af34139482e2fdaf324298e9de91
Craig Wilson <cw66786@gmail.com>
2023-04-30 06:20:09 -0500

    intellinsense not properly working

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,10 @@
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GamePanel extends JPANEL implements ActionListener {
     
+    @override
+    public void actionPerformed(ActionEvent e){
+        
+    }
 }
